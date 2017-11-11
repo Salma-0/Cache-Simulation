10fe153e87203670d26a2a5a56f77053611f010a
Salma <Salma@salmas-air.lan>
2017-11-11 14:01:58 +0800

    Initial Commit

---
+++ Cache Simulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cache Simulation
+//
+//  Created by Salma on 11/11/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
